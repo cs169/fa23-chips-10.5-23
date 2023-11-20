9e1f0060259092715c2a48fa7258e94a6d14b8bd
Xinze Guo <sunnyguo@berkeley.edu>
2023-11-20 07:01:36 +0000

    Half-way of part2, check rebase

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,4 +4,12 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+  
+  def show
+    @representative = Representative.find_by(params[:id])
+    if @representative.nil?
+      flash[:warning] = I18n.t(id_no_found)
+      redirect Representative_index_path
+    end
+  end
 end
