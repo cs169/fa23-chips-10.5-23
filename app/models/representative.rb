1521275010a5de88daea38dd38c8f7f418a560c1
gaoshh711 <gaoshh1@gmail.com>
2023-11-19 23:08:12 -0800

    Upd model representative 7

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,10 +2,16 @@
 
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
+  
+  # avoid existing representative
+   validates :name, uniqueness: { scope: [:ocdid, :title] }
 
   def self.civic_api_to_representative_params(rep_info)
     reps = []
-
+    # rep_info lack required info
+    if (not rep_info) or not rep_info.respond_to?(:officials) or not rep_info.respond_to?(:offices)
+      return reps
+    end
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
@@ -16,10 +22,17 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
+      
+      # no matched offices
+      if ocdid_temp=='' or title_temp==''
+        next
+      end
 
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
+      rep = Representative.new({ name: official.name, ocdid: ocdid_temp,
           title: title_temp })
-      reps.push(rep)
+      if rep.save
+        reps.push(rep)
+      end
     end
 
     reps
