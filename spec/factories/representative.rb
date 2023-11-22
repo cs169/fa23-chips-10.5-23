44e99c96fb7c04257f8ff22a9fc639135ed17940
gaoshh711 <gaoshh1@gmail.com>
2023-11-21 19:50:14 -0800

    merge factories and rename this file

--- spec/factories/representative.rb
+++ spec/factories/representative.rb
@@ -3,6 +3,8 @@
 FactoryBot.define do
   factory :representative do
     title { 'Governor of California' }
+    political_party { 'Democratic' }
+    contact_address { 'California' }
     name { 'Gavin Newsom' }
   end
 end
