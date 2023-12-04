c7ecdde9fdb53bed4c4f1b50e4496814592bdcff
Clarivy <zqc.sc.hn@gmail.com>
2023-12-04 13:48:11 -0800

    refactor: use official news api

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
-require 'open-uri'
+
+require 'news-api'
 
 class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
+
+  @news_api = News.new(Rails.application.credentials[Rails.env.to_sym][:NEWS_API_KEY])
 
   def self.find_for(representative_id)
     NewsItem.find_by(
@@ -11,33 +14,26 @@ class NewsItem < ApplicationRecord
     )
   end
 
-  def self.search_articles(rep_id, issue);
+  def self.search_articles(rep_id, issue)
     # Get the representative's name
     rep = Representative.find(rep_id)
     rep_name = rep.name
-    rep_name = rep_name.split(' ')
+    rep_name = rep_name.split
     # rep_name  = rep_name.map { |name| '"' + name + '"' } # Add '"' to name search exact name
     # Join the name
     rep_name = rep_name.join('+')
 
     # Get the issue
-    issue = issue.split(' ')
+    issue = issue.split
     # issue = issue.map { |issue| '"' + issue + '"' } # Add '"' to issue search exact issue
     issue = issue.join('+')
 
-    # Get API key
-    api_key = Rails.application.credentials[Rails.env.to_sym][:NEWS_API_KEY]
-
+    # Get the query
+    query = "#{rep_name}+#{issue}"
 
     # Get the articles
-    url = "https://newsapi.org/v2/everything?q=#{rep_name}+#{issue}&apiKey=#{api_key}&sortBy=relevancy&language=en"
-    response = open(url).read
-    response = JSON.parse(response)
-    response = response['articles']
+    @news_api.get_everything(q: query, language: 'en', sortBy: 'relevancy', page: 1, pageSize: 5)
 
-    # Return top 5 articles
-    response = response[0..4]
-
-    return response
+    # Return the articles
   end
 end
